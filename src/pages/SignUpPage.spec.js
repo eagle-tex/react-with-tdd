2b4d069292be2a5a62cf43ef5280f7a6f6cbe04d
Gildas Montcho <gmontcho@gmail.com>
2023-01-02 01:12:07 +0100

    style(SignUpPage): wrap test #1 inside 2 describe blocks (Sign Up Page > Layout)

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -1,9 +1,13 @@
 import SignUpPage from "./SignUpPage.jsx";
 import { render, screen } from "@testing-library/react";
 
-it("has header", () => {
-  render(<SignUpPage />);
-  const header = screen.queryByRole("heading", { name: "Sign Up" });
+describe("Sign Up Page", () => {
+  describe("Layout", () => {
+    it("has header", () => {
+      render(<SignUpPage />);
+      const header = screen.queryByRole("heading", { name: "Sign Up" });
 
-  expect(header).toBeInTheDocument();
+      expect(header).toBeInTheDocument();
+    });
+  });
 });
