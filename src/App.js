f6bb21639fd8a37b0ea81eca3729e7e094d5ab63
Gildas Montcho <gmontcho@gmail.com>
2023-01-04 00:06:02 +0100

    feat: add bootstrap class names to jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import SignUpPage from './pages/SignUpPage.jsx';
 
 function App() {
-  return <SignUpPage />;
+  return (
+    <div className="container">
+      <SignUpPage />
+    </div>
+  );
 }
 
 export default App;
