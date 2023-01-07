c8306344e8bd7cc216d271a4734d0fd710256d6f
Gildas Montcho <gmontcho@gmail.com>
2023-01-07 06:54:32 +0100

    test: add #16 'displays validation message for username'

--- src/pages/SignUpPage.spec.js
+++ src/pages/SignUpPage.spec.js
@@ -184,5 +184,27 @@ describe('Sign Up Page', () => {
       // NOTE: alternative way
       // await waitForElementToBeRemoved(form);
     });
+
+    it('displays validation message for username', async () => {
+      server.use(
+        rest.post('/api/1.0/users', (req, res, ctx) => {
+          return res(
+            ctx.status(400),
+            ctx.json({
+              validationErrors: { username: 'Username cannot be null' }
+            })
+          );
+        })
+      );
+
+      setup();
+      userEvent.click(button);
+
+      const validationError = await screen.findByText(
+        'Username cannot be null'
+      );
+
+      expect(validationError).toBeInTheDocument();
+    });
   });
 });
