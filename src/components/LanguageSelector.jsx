13c728b863b3b44afbcdd619337ac4312e1457f2
Gildas Montcho <gmontcho@gmail.com>
2023-01-08 22:50:36 +0100

    refactor(LanguageSelector): implement w/ useTranslation and w/o props/withTranslation

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,14 +1,15 @@
-import PropTypes from 'prop-types';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
-const LanguageSelector = props => {
+const LanguageSelector = () => {
+  const { i18n } = useTranslation();
+
   return (
     <>
       <a href="#" className="link-light">
         <img
           src="https://www.countryflagicons.com/FLAT/24/FR.png"
           title="French"
-          onClick={() => props.i18n.changeLanguage('fr')}
+          onClick={() => i18n.changeLanguage('fr')}
           alt="French Flag"
         />
       </a>
@@ -16,7 +17,7 @@ const LanguageSelector = props => {
         <img
           src="https://www.countryflagicons.com/FLAT/24/GB.png"
           title="English"
-          onClick={() => props.i18n.changeLanguage('en')}
+          onClick={() => i18n.changeLanguage('en')}
           alt="Great Britain Flag"
         />
       </a>
@@ -24,9 +25,4 @@ const LanguageSelector = props => {
   );
 };
 
-LanguageSelector.propTypes = {
-  // t: PropTypes.func,
-  i18n: PropTypes.object
-};
-
-export default withTranslation()(LanguageSelector);
+export default LanguageSelector;
