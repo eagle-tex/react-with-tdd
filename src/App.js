4dedc8c113c62a05b2d518cb2866ae879ddd8802
Gildas Montcho <gmontcho@gmail.com>
2023-01-10 06:40:15 +0100

    feat(App): pass #33 -> import HomePage comp. & render it in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import SignUpPage from './pages/SignUpPage.jsx';
+import HomePage from './pages/HomePage.jsx';
 import LanguageSelector from './components/LanguageSelector.jsx';
 
 function App() {
   return (
     <div className="container">
+      <HomePage />
       <SignUpPage />
       <LanguageSelector />
     </div>
