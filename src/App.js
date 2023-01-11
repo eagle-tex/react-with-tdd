84b5e0ebef7dd48d3d827325ed582aa4b2b82171
Gildas Montcho <gmontcho@gmail.com>
2023-01-11 05:16:33 +0100

    feat(App): pass #42 -> render UserPage component conditionally

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import SignUpPage from './pages/SignUpPage.jsx';
 import HomePage from './pages/HomePage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
+import UserPage from './pages/UserPage.jsx';
 import LanguageSelector from './components/LanguageSelector.jsx';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       {window.location.pathname === '/' && <HomePage />}
       {window.location.pathname === '/signup' && <SignUpPage />}
       {window.location.pathname === '/login' && <LoginPage />}
+      {window.location.pathname === '/user/1' && <UserPage />}
       <LanguageSelector />
     </div>
   );
