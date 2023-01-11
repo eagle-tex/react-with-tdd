a37867a94ec32909f65fbe515a17cdafe927dee8
Gildas Montcho <gmontcho@gmail.com>
2023-01-11 05:24:05 +0100

    test(App.spec): add #44-49 'does not display `page` when path is `path` & pass

--- src/App.spec.js
+++ src/App.spec.js
@@ -21,10 +21,16 @@ describe('Routing', () => {
     path         | pageTestId       | page
     ${'/'}       | ${'signup-page'} | ${'SignUpPage'}
     ${'/'}       | ${'login-page'}  | ${'LoginPage'}
+    ${'/'}       | ${'user-page'}   | ${'UserPage'}
     ${'/signup'} | ${'home-page'}   | ${'HomePage'}
     ${'/signup'} | ${'login-page'}  | ${'LoginPage'}
+    ${'/signup'} | ${'user-page'}   | ${'UserPage'}
     ${'/login'}  | ${'home-page'}   | ${'HomePage'}
     ${'/login'}  | ${'signup-page'} | ${'SignUpPage'}
+    ${'/login'}  | ${'user-page'}   | ${'UserPage'}
+    ${'/user/1'} | ${'home-page'}   | ${'HomePage'}
+    ${'/user/1'} | ${'signup-page'} | ${'SignUpPage'}
+    ${'/user/1'} | ${'login-page'}  | ${'LoginPage'}
   `('does not display $page when path is $path', ({ path, pageTestId }) => {
     window.history.pushState({}, '', path);
     render(<App />);
