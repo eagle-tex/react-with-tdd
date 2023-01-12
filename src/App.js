5931be3f61fe0a6747dd8f0170c4cb69ab3c0657
Gildas Montcho <gmontcho@gmail.com>
2023-01-12 07:07:38 +0100

    feat(App): pass #51 -> add a link to /signup w/ translated title & text content

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import HomePage from './pages/HomePage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import UserPage from './pages/UserPage.jsx';
 import LanguageSelector from './components/LanguageSelector.jsx';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t } = useTranslation();
   return (
     <div className="container">
       <div>
         <a href="/" title="Home">
           Hoaxify
+        </a>
+        <a href="/signup" title={t('signUp')}>
+          {t('signUp')}
         </a>
       </div>
       {window.location.pathname === '/' && <HomePage />}
