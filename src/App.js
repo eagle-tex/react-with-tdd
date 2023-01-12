254425240a221eea2d4bc3491cb4729198a3a248
Gildas Montcho <gmontcho@gmail.com>
2023-01-12 07:24:09 +0100

    feat(App): pass #52 -> add path to state, create onClickLink ev. hdler & update return

--- src/App.js
+++ src/App.js
@@ -4,23 +4,34 @@ import LoginPage from './pages/LoginPage.jsx';
 import UserPage from './pages/UserPage.jsx';
 import LanguageSelector from './components/LanguageSelector.jsx';
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 
 function App() {
   const { t } = useTranslation();
+
+  const [path, setPath] = useState(window.location.pathname);
+
+  const onClickLink = event => {
+    event.preventDefault();
+    const linkPath = event.target.attributes.href.value;
+    window.history.pushState({}, '', linkPath);
+    setPath(linkPath);
+  };
+
   return (
     <div className="container">
       <div>
         <a href="/" title="Home">
           Hoaxify
         </a>
-        <a href="/signup" title={t('signUp')}>
+        <a href="/signup" onClick={onClickLink} title={t('signUp')}>
           {t('signUp')}
         </a>
       </div>
-      {window.location.pathname === '/' && <HomePage />}
-      {window.location.pathname === '/signup' && <SignUpPage />}
-      {window.location.pathname === '/login' && <LoginPage />}
-      {window.location.pathname.startsWith('/user') && <UserPage />}
+      {path === '/' && <HomePage />}
+      {path === '/signup' && <SignUpPage />}
+      {path === '/login' && <LoginPage />}
+      {path.startsWith('/user') && <UserPage />}
       <LanguageSelector />
     </div>
   );
