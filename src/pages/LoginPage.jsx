8ba17545435b4f062d322e17ea802c79f4bf1a24
Gildas Montcho <gmontcho@gmail.com>
2023-01-13 23:03:17 +0100

    docs(LoginPage): demonstrate lifecycle hooks with functional components

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,25 @@
+import { useState, useEffect } from 'react';
+
 const LoginPage = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('increasing counter');
+      setCounter(previousCounter => {
+        return previousCounter + 1;
+      });
+    }, 1000);
+    console.log('this is like component did mount');
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div data-testid="login-page">
       <h1>Login Page</h1>
+      {counter}
     </div>
   );
 };
