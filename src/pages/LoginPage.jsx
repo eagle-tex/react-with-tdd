5b59c0c521ce9892088805b0992920b7196936fb
Gildas Montcho <gmontcho@gmail.com>
2023-01-14 07:07:57 +0100

    docs(LoginPage): remove state (counter) & 2 previous useEffect uses

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,28 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const LoginPage = props => {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      console.log('increasing counter');
-      setCounter(previousCounter => {
-        return previousCounter + 1;
-      });
-    }, 1000);
-    console.log('this is like component did mount');
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log(
-      'this is like component did update, but it is for specified dependency'
-    );
-  }, [counter]);
-
   useEffect(() => {
     console.log(`new random: ${props.random}`);
   }, [props.random]);
@@ -30,7 +9,6 @@ const LoginPage = props => {
   return (
     <div data-testid="login-page">
       <h1>Login Page</h1>
-      {counter}
     </div>
   );
 };
