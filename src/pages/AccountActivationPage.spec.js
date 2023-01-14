215a59fa19bbc1aaee52f487263391aab9665472
Gildas Montcho <gmontcho@gmail.com>
2023-01-14 07:48:38 +0100

    refactor(AccActivPage): #67 create a setup function to DRY test #67

--- src/pages/AccountActivationPage.spec.js
+++ src/pages/AccountActivationPage.spec.js
@@ -2,10 +2,13 @@ import { render, screen } from '@testing-library/react';
 import AccountActivationPage from './AccountActivationPage.jsx';
 
 describe('Account Activation Page', () => {
-  it('displays activation success message when token is valid', async () => {
-    const match = { params: { token: '1234' } };
-
+  const setup = token => {
+    const match = { params: { token } };
     render(<AccountActivationPage match={match} />);
+  };
+
+  it('displays activation success message when token is valid', async () => {
+    setup('1234');
     const message = await screen.findByText('Account is activated');
 
     expect(message).toBeInTheDocument();
