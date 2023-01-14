f5e81ea9fa088a21cd0b86fbf905186f07e6033d
Gildas Montcho <gmontcho@gmail.com>
2023-01-15 00:08:23 +0100

    feat(UserList): create a UserList component

---
+++ src/components/UserList.jsx
@@ -0,0 +1,11 @@
+const UserList = () => {
+  return (
+    <div className="card">
+      <div className="card-header text-center">
+        <h3>Users</h3>
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
