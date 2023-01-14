b82dd4da36e9574bdd1038ceadd4efe14f70bbb0
Gildas Montcho <gmontcho@gmail.com>
2023-01-14 08:44:44 +0100

    feat(AccActivPage): pass #68 -> show msg on activ. succ. w/ props & state - error msg

--- src/pages/AccountActivationPage.jsx
+++ src/pages/AccountActivationPage.jsx
@@ -1,9 +1,27 @@
-const AccountActivationPage = () => {
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { activate } from '../api/apiCalls';
+
+const AccountActivationPage = props => {
+  const [result, setResult] = useState();
+
+  useEffect(() => {
+    activate(props.match.params.token).then(() => {
+      setResult('success');
+    });
+  }, []);
+
   return (
     <div data-testid="activation-page">
-      <div className="alert alert-success mt-3">Account is activated</div>
+      {result === 'success' && (
+        <div className="alert alert-success mt-3">Account is activated</div>
+      )}
     </div>
   );
+};
+
+AccountActivationPage.propTypes = {
+  match: PropTypes.object
 };
 
 export default AccountActivationPage;
