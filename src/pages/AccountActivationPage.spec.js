92b8a76749577b7243d5242b98a42818b493cd61
Gildas Montcho <gmontcho@gmail.com>
2023-01-14 08:39:58 +0100

    test(AccActivPage.spec): add #68 'sends activ. request to backend' + setup server

--- src/pages/AccountActivationPage.spec.js
+++ src/pages/AccountActivationPage.spec.js
@@ -1,5 +1,24 @@
 import { render, screen } from '@testing-library/react';
 import AccountActivationPage from './AccountActivationPage.jsx';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
+let counter = 0;
+const server = setupServer(
+  rest.post('/api/1.0/users/token/:token', (req, res, ctx) => {
+    counter += 1;
+    return res(ctx.status(200));
+  })
+);
+
+beforeAll(() => server.listen());
+
+beforeEach(() => {
+  counter = 0;
+  server.resetHandlers();
+});
+
+afterAll(() => server.close());
 
 describe('Account Activation Page', () => {
   const setup = token => {
@@ -12,5 +31,12 @@ describe('Account Activation Page', () => {
     const message = await screen.findByText('Account is activated');
 
     expect(message).toBeInTheDocument();
+  });
+
+  it('sends activation request to backend', async () => {
+    setup('1234');
+    await screen.findByText('Account is activated');
+
+    expect(counter).toBe(1);
   });
 });
