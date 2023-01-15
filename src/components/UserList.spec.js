ee662f88e4ba569bc9e25bb66449b324bcdc498f
Gildas Montcho <gmontcho@gmail.com>
2023-01-15 08:44:37 +0100

    refactor(UserList.spec): pass #73 -> update/fix function getPage and its usage

--- src/components/UserList.spec.js
+++ src/components/UserList.spec.js
@@ -3,20 +3,41 @@ import UserList from './UserList.jsx';
 import { setupServer } from 'msw/node';
 import { rest } from 'msw';
 
-const page1 = {
-  content: [
-    { id: 1, username: 'user1', email: 'user1@mail.com', image: null },
-    { id: 2, username: 'user2', email: 'user2@mail.com', image: null },
-    { id: 3, username: 'user3', email: 'user3@mail.com', image: null }
-  ],
-  page: 0,
-  size: 3,
-  totalPages: 9
+const users = [
+  { id: 1, username: 'user1', email: 'user1@mail.com', image: null },
+  { id: 2, username: 'user2', email: 'user2@mail.com', image: null },
+  { id: 3, username: 'user3', email: 'user3@mail.com', image: null },
+  { id: 4, username: 'user4', email: 'user4@mail.com', image: null },
+  { id: 5, username: 'user5', email: 'user5@mail.com', image: null },
+  { id: 6, username: 'user6', email: 'user6@mail.com', image: null },
+  { id: 7, username: 'user7', email: 'user7@mail.com', image: null }
+];
+
+const getPage = (page, size) => {
+  const start = page * size;
+  const end = start + size;
+  const totalPages = Math.ceil(users.length / size);
+
+  return {
+    content: users.slice(start, end),
+    page,
+    size,
+    totalPages
+  };
 };
 
 const server = setupServer(
-  rest.get('/api/1.0/users', (_req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(page1));
+  rest.get('/api/1.0/users', (req, res, ctx) => {
+    let page = Number.parseInt(req.url.searchParams.get('page'));
+    let size = Number.parseInt(req.url.searchParams.get('size'));
+    if (Number.isNaN(page)) {
+      page = 0;
+    }
+    if (Number.isNaN(size)) {
+      size = 5;
+    }
+
+    return res(ctx.status(200), ctx.json(getPage(page, size)));
   })
 );
 
@@ -31,8 +52,8 @@ afterAll(() => server.close());
 describe('User List', () => {
   it('displays three users in list', async () => {
     render(<UserList />);
-    const users = await screen.findAllByText(/user/);
+    const usersList = await screen.findAllByText(/user/);
 
-    expect(users.length).toBe(3);
+    expect(usersList.length).toBe(3);
   });
 });
