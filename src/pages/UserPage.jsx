663b98f713d2e776fc50afa4dda1f63968c11d5f
Gildas Montcho <gmontcho@gmail.com>
2023-01-18 07:09:03 +0100

    feat(UserPage): pass #84 -> add pendingApiCall to state, render ProfCard/Spinner cndtnally

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -2,27 +2,39 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getUserById } from '../api/apiCalls';
 import ProfileCard from '../components/ProfileCard.jsx';
+import Spinner from '../components/Spinner.jsx';
+import Alert from '../components/Alert.jsx';
 
 class UserPage extends Component {
   state = {
-    user: {}
+    user: {},
+    pendingApiCall: false
   };
 
   async componentDidMount() {
+    this.setState({ pendingApiCall: true });
+
     try {
       const response = await getUserById(this.props.match.params.id);
       this.setState({ user: response.data });
     } catch (error) {
       // empty
     }
+
+    this.setState({ pendingApiCall: false });
   }
 
   render() {
-    const { user } = this.state;
+    const { user, pendingApiCall } = this.state;
 
     return (
       <div data-testid="user-page">
-        <ProfileCard user={user} />
+        {!pendingApiCall && <ProfileCard user={user} />}
+        {pendingApiCall && (
+          <Alert type="secondary" center>
+            <Spinner size="big" />
+          </Alert>
+        )}
       </div>
     );
   }
