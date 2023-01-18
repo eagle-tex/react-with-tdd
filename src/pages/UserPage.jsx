6b69507f404730435c28f762eb0b887a74d7b5be
Gildas Montcho <gmontcho@gmail.com>
2023-01-18 06:34:58 +0100

    feat(UserPage): pass #83 -> conv. to class cmp. +state +API call in compDidMount - issues WARNING

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,9 +1,30 @@
-const UserPage = () => {
-  return (
-    <div data-testid="user-page">
-      <h1>User Page</h1>
-    </div>
-  );
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getUserById } from '../api/apiCalls';
+
+class UserPage extends Component {
+  state = {
+    user: {}
+  };
+
+  async componentDidMount() {
+    try {
+      const response = await getUserById(this.props.match.params.id);
+      this.setState({ user: response.data });
+    } catch (error) {
+      // empty
+    }
+  }
+
+  render() {
+    const { user } = this.state;
+
+    return <div data-testid="user-page">{user.username}</div>;
+  }
+}
+
+UserPage.propTypes = {
+  match: PropTypes.object
 };
 
 export default UserPage;
