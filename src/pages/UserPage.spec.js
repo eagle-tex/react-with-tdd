e29fbad9feb8ec1bc2c5badd67a49a4c86ae816c
Gildas Montcho <gmontcho@gmail.com>
2023-01-18 06:31:37 +0100

    test(UserPage): add #83 'User Page' > 'displays user name on page when user found'

---
+++ src/pages/UserPage.spec.js
@@ -0,0 +1,40 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import UserPage from './UserPage.jsx';
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
+const server = setupServer();
+
+beforeAll(() => server.listen());
+
+beforeEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => server.close());
+
+describe('User Page', () => {
+  beforeEach(() => {
+    server.use(
+      rest.get('/api/1.0/users/:id', (req, res, ctx) => {
+        return res(
+          ctx.json({
+            id: 1,
+            username: 'user1',
+            email: 'user1@mail.com',
+            image: null
+          })
+        );
+      })
+    );
+  });
+
+  it('displays user name on page when user found', async () => {
+    const match = { params: { id: 1 } };
+    render(<UserPage match={match} />);
+
+    await waitFor(() => {
+      expect(screen.queryByText('user1')).toBeInTheDocument();
+    });
+  });
+});
