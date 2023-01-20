28c6ebe0acf50615833ab35bd25149785bdcff6a
Gildas Montcho <gmontcho@gmail.com>
2023-01-20 21:58:53 +0100

    feat(App): #105 - wrong solution

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import LanguageSelector from './components/LanguageSelector.jsx';
 import { useTranslation } from 'react-i18next';
 import logo from './assets/hoaxify.png';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
   const { t } = useTranslation();
 
   return (
@@ -20,19 +22,27 @@ function App() {
             Hoaxify
           </Link>
           <ul className="navbar-nav">
-            <Link className="nav-link" to="/signup" title={t('signUp')}>
-              {t('signUp')}
-            </Link>
-            <Link className="nav-link" to="/login" title="Login">
-              Login
-            </Link>
+            {!isLoggedIn && (
+              <>
+                <Link className="nav-link" to="/signup" title={t('signUp')}>
+                  {t('signUp')}
+                </Link>
+                <Link className="nav-link" to="/login" title="Login">
+                  Login
+                </Link>
+              </>
+            )}
           </ul>
         </div>
       </nav>
       <div className="container pt-3">
         <Route exact path="/" component={HomePage} />
         <Route path="/signup" component={SignUpPage} />
-        <Route path="/login" component={LoginPage} />
+        {/* <Route path="/login" component={LoginPage} /> */}
+        {/* The following solution is WRONG */}
+        <Route path="/login">
+          <LoginPage onLoginSuccess={() => setLoggedIn(true)} />
+        </Route>
         <Route path="/user/:id" component={UserPage} />
         <Route path="/activate/:token" component={AccountActivationPage} />
         <LanguageSelector />
