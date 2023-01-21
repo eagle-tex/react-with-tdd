6109e9ae8ef426f3a419bde5af70f15e4064ea1a
Gildas Montcho <gmontcho@gmail.com>
2023-01-21 22:17:01 +0100

    refactor(NavBar): get AuthContext from AuthContextWrapper instead of from App

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import logo from '../assets/hoaxify.png';
-import { AuthContext } from '../App';
+import { AuthContext } from '../state/AuthContextWrapper';
 import { useContext } from 'react';
 
 const NavBar = () => {
