d6ed1d5b7cd1bbd32c52c7e3cfb83503a66c6e10
Gildas Montcho <gmontcho@gmail.com>
2023-01-22 00:18:40 +0100

    feat(setup): create a setup module w/ RootWrapper (as per RTL documentation)

---
+++ src/test/setup.js
@@ -0,0 +1,27 @@
+// NOTE: https://testing-library.com/docs/react-testing-library/setup
+import React from 'react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import AuthContextWrapper from '../state/AuthContextWrapper';
+
+const RootWrapper = ({ children }) => {
+  return (
+    <Router theme="light">
+      <AuthContextWrapper>{children}</AuthContextWrapper>
+    </Router>
+  );
+};
+
+RootWrapper.propTypes = {
+  children: PropTypes.element
+};
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: RootWrapper, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { customRender as render };
