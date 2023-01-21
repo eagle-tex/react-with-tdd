0a15b7cd94709983520f02c3beefcbaac786f840
Gildas Montcho <gmontcho@gmail.com>
2023-01-21 22:16:18 +0100

    refactor(App): delete AuthContext.Provider from App

--- src/App.js
+++ src/App.js
@@ -6,36 +6,20 @@ import AccountActivationPage from './pages/AccountActivationPage.jsx';
 import LanguageSelector from './components/LanguageSelector.jsx';
 import NavBar from './components/NavBar';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { useState, createContext } from 'react';
-
-export const AuthContext = createContext();
 
 function App() {
-  const [auth, setAuth] = useState({
-    isLoggedIn: false,
-    id: ''
-  });
-
   return (
-    <AuthContext.Provider
-      value={{
-        isLoggedIn: auth.isLoggedIn,
-        id: auth.id,
-        onLoginSuccess: setAuth
-      }}
-    >
-      <Router>
-        <NavBar />
-        <div className="container pt-3">
-          <Route exact path="/" component={HomePage} />
-          <Route path="/signup" component={SignUpPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/user/:id" component={UserPage} />
-          <Route path="/activate/:token" component={AccountActivationPage} />
-          <LanguageSelector />
-        </div>
-      </Router>
-    </AuthContext.Provider>
+    <Router>
+      <NavBar />
+      <div className="container pt-3">
+        <Route exact path="/" component={HomePage} />
+        <Route path="/signup" component={SignUpPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/user/:id" component={UserPage} />
+        <Route path="/activate/:token" component={AccountActivationPage} />
+        <LanguageSelector />
+      </div>
+    </Router>
   );
 }
 
