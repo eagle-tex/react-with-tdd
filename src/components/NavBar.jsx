93762b4966ec6636b61cabc70ddb9caff6d69c45
Gildas Montcho <gmontcho@gmail.com>
2023-01-21 17:29:54 +0100

    docs(NavBar): state management w/ AuthContext.Consumer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,41 +2,45 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import logo from '../assets/hoaxify.png';
+import { AuthContext } from '../App';
 
 const NavBar = props => {
-  const { auth } = props;
   const { t } = useTranslation();
 
   return (
-    <nav className="navbar navbar-expand navbar-light bg-light shadow">
-      <div className="container">
-        <Link className="navbar-brand" to="/" title="Home">
-          <img src={logo} alt="Hoaxify" width="60" />
-          Hoaxify
-        </Link>
-        <ul className="navbar-nav">
-          {!auth.isLoggedIn && (
-            <>
-              <Link className="nav-link" to="/signup" title={t('signUp')}>
-                {t('signUp')}
-              </Link>
-              <Link className="nav-link" to="/login" title="Login">
-                {t('login')}
-              </Link>
-            </>
-          )}
-          {auth.isLoggedIn && (
-            <Link
-              className="nav-link"
-              to={`/user/${auth.id}`}
-              title="My Profile"
-            >
-              My Profile
+    <AuthContext.Consumer>
+      {auth => (
+        <nav className="navbar navbar-expand navbar-light bg-light shadow">
+          <div className="container">
+            <Link className="navbar-brand" to="/" title="Home">
+              <img src={logo} alt="Hoaxify" width="60" />
+              Hoaxify
             </Link>
-          )}
-        </ul>
-      </div>
-    </nav>
+            <ul className="navbar-nav">
+              {!auth.isLoggedIn && (
+                <>
+                  <Link className="nav-link" to="/signup" title={t('signUp')}>
+                    {t('signUp')}
+                  </Link>
+                  <Link className="nav-link" to="/login" title="Login">
+                    {t('login')}
+                  </Link>
+                </>
+              )}
+              {auth.isLoggedIn && (
+                <Link
+                  className="nav-link"
+                  to={`/user/${auth.id}`}
+                  title="My Profile"
+                >
+                  My Profile
+                </Link>
+              )}
+            </ul>
+          </div>
+        </nav>
+      )}
+    </AuthContext.Consumer>
   );
 };
 
