cbfc45ac5332ac1b40c784d2cf9bf334a68b7936
Gildas Montcho <gmontcho@gmail.com>
2023-01-21 22:05:42 +0100

    fix(index): fix index.js to be compliant with react v18

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import reportWebVitals from './reportWebVitals';
 import './locale/i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>, document.getElementById('root')
-// );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
