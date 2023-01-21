4b0aac4afc09221edc72df0364fd52dcbd12146c
Gildas Montcho <gmontcho@gmail.com>
2023-01-21 17:44:55 +0100

    refactor(NavBar): get auth from useContext(AuthContext) & update AuthCtx.Consumer

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,51 +1,44 @@
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import logo from '../assets/hoaxify.png';
 import { AuthContext } from '../App';
+import { useContext } from 'react';
 
-const NavBar = props => {
+const NavBar = () => {
   const { t } = useTranslation();
+  const auth = useContext(AuthContext);
 
   return (
-    <AuthContext.Consumer>
-      {auth => (
-        <nav className="navbar navbar-expand navbar-light bg-light shadow">
-          <div className="container">
-            <Link className="navbar-brand" to="/" title="Home">
-              <img src={logo} alt="Hoaxify" width="60" />
-              Hoaxify
+    <nav className="navbar navbar-expand navbar-light bg-light shadow">
+      <div className="container">
+        <Link className="navbar-brand" to="/" title="Home">
+          <img src={logo} alt="Hoaxify" width="60" />
+          Hoaxify
+        </Link>
+        <ul className="navbar-nav">
+          {!auth.isLoggedIn && (
+            <>
+              <Link className="nav-link" to="/signup" title={t('signUp')}>
+                {t('signUp')}
+              </Link>
+              <Link className="nav-link" to="/login" title="Login">
+                {t('login')}
+              </Link>
+            </>
+          )}
+          {auth.isLoggedIn && (
+            <Link
+              className="nav-link"
+              to={`/user/${auth.id}`}
+              title="My Profile"
+            >
+              My Profile
             </Link>
-            <ul className="navbar-nav">
-              {!auth.isLoggedIn && (
-                <>
-                  <Link className="nav-link" to="/signup" title={t('signUp')}>
-                    {t('signUp')}
-                  </Link>
-                  <Link className="nav-link" to="/login" title="Login">
-                    {t('login')}
-                  </Link>
-                </>
-              )}
-              {auth.isLoggedIn && (
-                <Link
-                  className="nav-link"
-                  to={`/user/${auth.id}`}
-                  title="My Profile"
-                >
-                  My Profile
-                </Link>
-              )}
-            </ul>
-          </div>
-        </nav>
-      )}
-    </AuthContext.Consumer>
+          )}
+        </ul>
+      </div>
+    </nav>
   );
-};
-
-NavBar.propTypes = {
-  auth: PropTypes.object
 };
 
 export default NavBar;
