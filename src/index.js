e4874406531e9d903b1022d196c7e2999471f1a9
Gildas Montcho <gmontcho@gmail.com>
2023-01-22 23:21:11 +0100

    fix(index): fix bug w/ React Router v5 (move back to NOT react v18 compliant)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './locale/i18n';
@@ -7,15 +7,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import createStore from './state/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Provider store={createStore()}>
         <App />
       </Provider>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
