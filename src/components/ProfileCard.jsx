59d6405e9c26f24f36c55b5a2706107430c86c44
Gildas Montcho <gmontcho@gmail.com>
2023-01-25 22:58:25 +0100

    refactor(ProfileCard): separate content to make the test more readable

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -10,6 +10,32 @@ const ProfileCard = props => {
   const { user } = props;
   const id = useSelector(store => store.id);
 
+  let content; // undefined
+
+  if (inEditMode) {
+    content = (
+      <>
+        <Input label="Change your username" id="username" />
+        <button className="btn btn-primary">Save</button>{' '}
+        <button className="btn btn-outline-secondary">Cancel</button>
+      </>
+    );
+  } else {
+    content = (
+      <>
+        <h3>{user.username}</h3>
+        {user.id === id && (
+          <button
+            className="btn btn-outline-success"
+            onClick={() => setEditMode(true)}
+          >
+            Edit
+          </button>
+        )}
+      </>
+    );
+  }
+
   return (
     <div className="card text-center">
       <div className="card-header">
@@ -21,24 +47,7 @@ const ProfileCard = props => {
           className="rounded-circle shadow"
         />{' '}
       </div>
-      <div className="card-body">
-        {!inEditMode && <h3>{user.username}</h3>}
-        {user.id === id && !inEditMode && (
-          <button
-            className="btn btn-outline-success"
-            onClick={() => setEditMode(true)}
-          >
-            Edit
-          </button>
-        )}
-        {inEditMode && (
-          <>
-            <Input label="Change your username" id="username" />
-            <button className="btn btn-primary">Save</button>{' '}
-            <button className="btn btn-outline-secondary">Cancel</button>
-          </>
-        )}
-      </div>
+      <div className="card-body">{content}</div>
     </div>
   );
 };
