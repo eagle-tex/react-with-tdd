450ffa32e477d2783ea02c7634da0d614a59fe49
Gildas Montcho <gmontcho@gmail.com>
2023-01-25 06:51:24 +0100

    feat(ProfileCard): pass #115 -> add pce of state/ev. handler to ProfCard & disp cdnally

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,8 +1,12 @@
 import PropTypes from 'prop-types';
 import defaultProfileImage from '../assets/profile.png';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import Input from './Input';
 
 const ProfileCard = props => {
+  const [inEditMode, setEditMode] = useState(false);
+
   const { user } = props;
   const id = useSelector(store => store.id);
 
@@ -11,7 +15,7 @@ const ProfileCard = props => {
       <div className="card-header">
         <img
           src={defaultProfileImage}
-          alt="profile image"
+          alt="profile"
           width="200"
           height="200"
           className="rounded-circle shadow"
@@ -20,8 +24,14 @@ const ProfileCard = props => {
       <div className="card-body">
         <h3>{user.username}</h3>
         {user.id === id && (
-          <button className="btn btn-outline-success">Edit</button>
+          <button
+            className="btn btn-outline-success"
+            onClick={() => setEditMode(true)}
+          >
+            Edit
+          </button>
         )}
+        {inEditMode && <Input label="Change your username" id="username" />}
       </div>
     </div>
   );
