96283b7459272201f68b292d501927432b75bf5f
Gildas Montcho <gmontcho@gmail.com>
2023-01-25 06:23:05 +0100

    refactor(ProfileCard.spec): create a setup function to DRY our tests

--- src/components/ProfileCard.spec.js
+++ src/components/ProfileCard.spec.js
@@ -3,18 +3,20 @@ import ProfileCard from './ProfileCard';
 import storage from '../state/storage';
 
 describe('Profile Card', () => {
-  it('displays Edit button when logged in user is shown on card', () => {
-    const user = { id: 5, username: 'user5' };
-    storage.setItem('auth', user);
+  const setup = (user = { id: 5, username: 'user5' }) => {
+    const LOGGED_IN_USER_IN_TEST = { id: 5, username: 'user5' }; // not necessarily default user
+    storage.setItem('auth', LOGGED_IN_USER_IN_TEST);
     render(<ProfileCard user={user} />);
+  };
+
+  it('displays Edit button when logged in user is shown on card', () => {
+    setup();
 
     expect(screen.getByRole('button', { name: 'Edit' })).toBeInTheDocument();
   });
 
   it('does not display Edit button for another user', () => {
-    const user = { id: 5, username: 'user5' };
-    storage.setItem('auth', user);
-    render(<ProfileCard user={{ id: 2, username: 'user2' }} />);
+    setup({ id: 2, username: 'user2' });
 
     expect(
       screen.queryByRole('button', { name: 'Edit' })
