1a955c978c98cf675bf51ea6fd415d169f5d9afe
Gildas Montcho <gmontcho@gmail.com>
2023-01-29 00:39:09 +0100

    feat(ProfileCard): pass #139 -> add a modal from bootstrap 5.3 (always displayed)

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -84,18 +84,54 @@ const ProfileCard = props => {
   }
 
   return (
-    <div className="card text-center">
-      <div className="card-header">
-        <img
-          src={defaultProfileImage}
-          alt="profile"
-          width="200"
-          height="200"
-          className="rounded-circle shadow"
-        />{' '}
+    <>
+      <div className="card text-center">
+        <div className="card-header">
+          <img
+            src={defaultProfileImage}
+            alt="profile"
+            width="200"
+            height="200"
+            className="rounded-circle shadow"
+          />{' '}
+        </div>
+        <div className="card-body">{content}</div>
       </div>
-      <div className="card-body">{content}</div>
-    </div>
+      <div
+        className="modal show d-block bg-black bg-opacity-50"
+        tabIndex="-1"
+        data-testid="modal"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Modal title</h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <p>Modal body text goes here.</p>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" className="btn btn-primary">
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
