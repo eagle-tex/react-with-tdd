f3dac7fff704535805e42ac2404f083ad4a5062a
Gildas Montcho <gmontcho@gmail.com>
2023-01-29 14:13:16 +0100

    refactor(authReducer): get/use action types from AUTH object

--- src/state/authReducer.js
+++ src/state/authReducer.js
@@ -1,14 +1,16 @@
+import { AUTH } from './constants';
+
 const reducer = (state, action) => {
-  if (action.type === 'login-success') {
+  if (action.type === AUTH.LOGIN_SUCCESS) {
     // WARN: do a DEEP COPY of the state object if state has
     //   nested object(s). Here, as it does not, a simple copy is OK
     return { ...state, ...action.payload, isLoggedIn: true };
-  } else if (action.type === 'user-update-success') {
+  } else if (action.type === AUTH.USER_UPDATE_SUCCESS) {
     return {
       ...state,
       username: action.payload.username
     };
-  } else if (action.type === 'logout-success') {
+  } else if (action.type === AUTH.LOGOUT_SUCCESS) {
     return {
       isLoggedIn: false
     };
